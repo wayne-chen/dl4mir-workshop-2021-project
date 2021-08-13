61225198f1d67142e9b1b74606016807b82adb65
Chen, Wayne <wayne.chen@dolby.com>
2021-08-12 23:53:13 -0700

    updating to work with mido

--- merge_midis.py
+++ merge_midis.py
@@ -1,4 +1,4 @@
-import midi
+import mido
 import pathlib
 
 # wavpath = Path("~/Downloads/DSD100subset/Sources/Dev/081 - Patrick Talbot - Set Me Free")
@@ -6,12 +6,12 @@ wavpath = pathlib.Path("/Users/wichen/Downloads/")
 
 filenames = ['drums.midi', 'guitar.midi', 'prediction.midi']
 
-pattern = midi.Pattern()
+merged_midi = mido.MidiFile(type=1)
 
-for f in filenames:
-    filepath = wavpath / f
-    current_pattern = midi.read_midifile(str(filepath))
-    for track in current_pattern:
-        pattern.append(track)
+appended_filenames = []
+[appended_filenames.append(mido.MidiFile(wavpath / f)) for f in filenames]
+merged_midi.ticks_per_beat = appended_filenames[0].ticks_per_beat
 
-midi.write_midifile('mixed.mid', pattern)
+for f in appended_filenames:
+    merged_midi.tracks += f.tracks
+merged_midi.save('merged.mid')
